834b9537b12bd4b8faf160ba0f61c8b8caf19106
leico <yoshitaka_12@iamas.ac.jp>
2016-09-07 01:00:11 +0900

    大幅改良 ========
    動作する状態になっている
    ----------
    
    より汎用的に
    -------
    文字列だけではなく、色々なものを使える
    ofxSimpleTimer<string>
    ofxSimpleTimer<int>
    
    隠匿性と使い勝手を高めている
    --------
    メインの部分でaddListenerを呼ぶ必要がない
    
    newしなくてもよい
    -------
    oFっぽくSetupで初期化
    
    Clear追加
    ------
    イベント処理の消去用
    
    変数名を変更
    ------------
    
    NAME -> id
    が一番顕著

--- src/ofxSimpleTimer.h
+++ src/ofxSimpleTimer.h
@@ -10,52 +10,313 @@
 
 #include "ofMain.h"
 
-class ofxSimpleTimer : public ofBaseApp
-{
+template<typename T>
+class ofxSimpleTimer{
+
+private:
+
+  static ofEvent<T> alerm;
+
+  enum STATUS{
+    STOP,
+    RUN,
+    PAUSE
+  } status;
+
+  struct TIME{
+    float current;
+    float start;
+    float end;
+    float pause;
+  } time;
+
+  struct LOOPCOUNT{
+    int current;
+    int end;
+  } loopcount;
+
+  T id;
+
     
 public:
     
     ofxSimpleTimer();
     ~ofxSimpleTimer();
-    
-    static ofEvent<string> TIMER_COMPLETE;
 
-    int getLoopCurrentCount();
-    int getLoopTotalCount();
-    float getCurrentTime();
-    float getTotalTime();
+    template<class ListenerClass>
+    void Setup(const T& _id, ListenerClass* listener, void (ListenerClass::* ListenerMethod)(T&));
+
+    template<class ListenerClass>
+    void Clear(ListenerClass* listener, void (ListenerClass::* ListenerMethod)(T&));
+
+    const int LoopCount_Current(void);
+    const int LoopCount_End    (void);
     
-    void setName(string name);
-    void setTime(int time, int loopCount);
+    const float CurrentTime(void);
+    const float EndTime    (void);
+
+          void ID(const T& _name);
+    const T&   ID(void);
+
+    void Limit(const int endtime_msec, const int endloopcount);
+
+    void        Start(const int endtime_msec, const int endloopcount = 1);
+    void        Stop (void);
+    void        Reset(void);
+    const float Pause(void); //when restart, return distance time from pause
+
+    const float Update(void);
+
     
-    void debugStart();
-    void debugStop();
-    void pause();
-    void reset();
-    void start();
     
-    void update();
     void drawStatus(ofPoint pos);
     
 private:
-    
-    void dispatch();
-    
-    bool UPDATE_ENABLED;
-    bool PAUSE;
-    bool DEBUG_ENABLED;
-    int LOOP_TOTAL;
-    string NAME;
-    
-    float _timer;
-    float _timer_startTime;
-    float _timer_endTime;
-    float _timer_pauseTime;
-    float _timer_pauseDistanceTime;
-    bool _timer_reached;
-    
-    int _loopCounter;    
-    
+
+    void Report(void);
 };
+
+
+template<typename T>
+ofEvent<T> ofxSimpleTimer<T> :: alerm = ofEvent<T>();
+
+/* ========================================================= *
+ * constructor/destructor                                    *
+ * ========================================================= */
+template<typename T>
+ofxSimpleTimer<T> :: ofxSimpleTimer() : 
+  loopcount({0}),
+  time     ({0}),
+  id       (),
+  status   (STATUS :: STOP)
+{}
+
+template<typename T>
+ofxSimpleTimer<T> :: ~ofxSimpleTimer(){}
+
+
+
+/* ========================================================= *
+ * const int CurrentLoopCount(void)                          *
+ * ========================================================= */
+template<typename T>
+const int ofxSimpleTimer<T> :: LoopCount_Current(void){
+  return loopcount.current;
+}
+
+/* ========================================================= *
+ * const int TotalLoopCount(void)                            *
+ * ========================================================= */
+template<typename T>
+const int ofxSimpleTimer<T> :: LoopCount_End(void){
+  return loopcount.end;
+}
+
+/* ========================================================= *
+ * const float CurrentTime(void) (ms)relative                *
+ * ========================================================= */
+template<typename T>
+const float ofxSimpleTimer<T> :: CurrentTime(void){
+  return Update();
+}
+
+/* ========================================================= *
+ * const float EndTime(void) (ms)absolute                    *
+ * ========================================================= */
+template<typename T>
+const float ofxSimpleTimer<T> :: EndTime(void){
+  return time.start + time.end;
+}
+
+
+
+
+
+/* ========================================================= *
+ * void                 ID(const std :: string _name)      *
+ * const std :: string& ID(void)
+ * ========================================================= */
+template<typename T>       void ofxSimpleTimer<T> :: ID(const T& _id){ id =  _id; }
+template<typename T> const T&   ofxSimpleTimer<T> :: ID(void)        { return id; }
+
+/* =========================================================== *
+ * void Limit(const int endtime_msec, const int endloopcount)  *
+ * =========================================================== */
+template<typename T>
+void ofxSimpleTimer<T> :: Limit(const int endtime_msec, const int endloopcount){
+
+  time.end      = endtime_msec;
+  loopcount.end = endloopcount;
+  return;
+}
+
+
+
+
+
+
+/* ========================================================= *
+ * const float Pause(void)                                   *
+ * ========================================================= */
+template<typename T>
+const float ofxSimpleTimer<T> :: Pause(void){
+  
+  if(status == STATUS :: STOP) return 0;
+
+  //-----------------pause or running
+ 
+  //when pausing, restart
+  if(status == STATUS :: PAUSE){
+
+    status     = STATUS :: RUN;
+    float dtfp = ofGetElapsedTimeMillis() - time.pause;
+    time.start     += dtfp;
+    return dtfp;
+  }
+
+  //-----------------running
+  time.pause   = ofGetElapsedTimeMillis();
+  time.current = time.pause;
+  status       = STATUS :: PAUSE;
+
+  return 0;
+}
+
+/* ========================================================= *
+ * void Stop(void)                                           *
+ * ========================================================= */
+template<typename T>
+void ofxSimpleTimer<T> :: Stop(void){
+  
+  if(status == STATUS :: STOP) return;
+
+  status    = STATUS :: STOP;
+  time      = {0};
+  loopcount = {0};
+  
+}
+
+/* ========================================================= *
+ * void Reset(void)                                          *
+ * ========================================================= */
+template<typename T>
+void ofxSimpleTimer<T> :: Reset(void){
+
+  if(status == STATUS :: STOP) return;
+
+  time.start        = ofGetElapsedTimeMillis();
+  time.current      = time.start;
+  if(status == STATUS :: PAUSE) time.pause = time.current;
+  loopcount.current = 0;
+}
+
+/* ========================================================= *
+ * void Start(const int endtime_msec, const int endloopoint) *
+ * ========================================================= */
+template<typename T>
+void ofxSimpleTimer<T> :: Start(const int endtime_msec, const int endloopcount){
+  
+  if(status != STATUS :: STOP) Stop();
+
+  Limit(endtime_msec, endloopcount);
+
+  status       = STATUS :: RUN;
+  time.start   = ofGetElapsedTimeMillis();
+  time.current = time.start;
+
+  return;
+  
+}
+
+/* ========================================================= *
+ * void Update(void)                                         *
+ * ========================================================= */
+template<typename T>
+const float ofxSimpleTimer<T> :: Update(void){
+
+
+  if(status != STATUS :: RUN) return time.current - time.start;
+
+  //-----------------only running
+
+
+  time.current = ofGetElapsedTimeMillis();
+  float end    = time.start + time.end;
+
+  while( time.current >= end ){
+    time.start   += time.end;
+    loopcount.current ++;
+
+    time.current = ofGetElapsedTimeMillis();
+    end          = time.start + time.end;
+
+
+    Report();
+    if(loopcount.end == 0) continue;
+
+    if(loopcount.current >= loopcount.end){
+      Stop();
+      return time.current;
+    }
+  }
+
+  return time.current - time.start;
+}
+
+//--------------------------------------------------------------
+// draw
+//--------------------------------------------------------------
+template<typename T>
+void ofxSimpleTimer<T>::drawStatus(ofPoint pos)
+{
+  int bar_width = 250;
+  int bar_height = 5;
+
+  ofSetColor(255);
+  ofDrawBitmapStringHighlight(ID() + " Set Time(ms) : "+ofToString( EndTime() ), pos.x, pos.y);
+  ofDrawBitmapStringHighlight(ID() + " Current Status(ms) : "+ofToString( CurrentTime() )+" / "+ofToString( time.end ), pos.x, pos.y+20);
+
+
+  ofSetColor(0);
+  ofNoFill();
+  ofDrawRectangle(pos.x, pos.y+35, bar_width, bar_height);
+
+}
+
+/* ========================================================= *
+ * void Report(void)                                         *
+ * ========================================================= */
+template<typename T>
+void ofxSimpleTimer<T> :: Report(void){
+  T id = ID();
+  ofNotifyEvent( alerm, id );
+}
+
+
+/* ================================================================= *
+ * void Setup(ListenerClass *listener, ListenerClassMethod *method); *
+ * void ListenerClassMethod(ArgumentType&);                          *
+ * ================================================================= */
+template<typename T>
+template<class ListenerClass>
+void ofxSimpleTimer<T> :: Setup( const T&             id
+                               ,       ListenerClass *listener
+                               ,       void           (ListenerClass :: *ListenerMethod)(T&)){
+  ID(id);
+  ofAddListener(alerm, listener, ListenerMethod);
+
+}
+/* ================================================================= *
+ * void Clear(ListenerClass *listener, ListenerClassMethod *method); *
+ * void ListenerClassMethod(ArgumentType&);                          *
+ * ================================================================= */
+template<typename T>
+template<class ListenerClass>
+void ofxSimpleTimer<T> :: Clear( ListenerClass *listener
+                            , void           (ListenerClass :: *ListenerMethod)(T&)){
+  ofRemoveListener(alerm, listener, ListenerMethod);
+
+}
+
 
 #endif
